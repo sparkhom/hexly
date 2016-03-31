13f1a95d45f65b24defe0e4dbeb2b2618fb025ae
Serguey Parkhomovsky <xindigo@gmail.com>
2016-03-31 15:56:45 -0700

    movement selection, etc

--- app.js
+++ app.js
@@ -1,32 +1,35 @@
 "use strict";
 
 var canvas = document.getElementById("game");
+var ctx = canvas.getContext("2d");
 var globalWidth = window.innerWidth;
 var globalHeight = window.innerHeight;
 var ratio = 1;
 var debug = true;
 var timeToDraw = 0;
-var ctx = canvas.getContext("2d");
 
-var hexSize = new Point(50,50);
+var keyCodes = {};
+
+var hexSize = new Point(30,30);
 var hexHeight = hexSize.y * 2;
 var hexWidth = Math.sqrt(3) / 2 * hexHeight;
 var vertDistance = hexHeight * 3/4;
 var horizDistance = hexWidth;
 var showCoordsText = false;
 
-var mapstore = [['W','W','W','W','W'],
-                ['W','W','W','W','W'],
-                ['W','W','L','L','W'],
-                ['W','W','L','L','W'],
-                ['W','W','L','L','L'],
-                ['W','W','L','L','L']];
+var currentCell = false;
+var selectedCell = false;
+var needsUpdate = false;
+
+var mapstore = [];
+generateRandomMap();
 var initmap = new Map(mapstore);
 
 var worldHeight = initmap.height * vertDistance;
 var worldWidth = initmap.width * horizDistance;
 var oldPos = new Point(0,0);
 var mouseStart = new Point(0,0);
+var mouseDrag = false;
 
 var layout = new Layout(layout_pointy, hexSize, new Point(hexSize.x, hexSize.y));
 
@@ -34,12 +37,16 @@ setup();
 
 function setup() {
     if (ctx) {
+        needsUpdate = true;
         init();
         requestAnimationFrame(update);
-        canvas.onmouseover = showMouseOver;
-        canvas.onmouseout = showMouseOut;
+        canvas.onmouseover = mouseOver;
+        canvas.onmouseout = mouseOut;
+        canvas.onmousemove = mouseMove;
         canvas.onmousedown = mouseDown;
         canvas.onmouseup = mouseUp;
+        window.onkeydown = keyDown;
+        window.onkeyup = keyUp;
         window.onresize = resize;
         resize();
     }
@@ -57,14 +64,20 @@ function update() {
 }
 
 function draw() {
+    if (!needsUpdate)
+        return;
     var startDraw = Date.now();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (var r = 0; r < initmap.height; r++) {
         var r_offset = Math.floor(r/2);
         for (var q = -r_offset; q < initmap.width - r_offset; q++) {
-            var type = initmap.get(q, r);
-            var hex = new Hex(q, r, -q-r);
+            var hex = initmap.get(q, r);
+            var cell = hex;
             var center = hex_to_pixel(layout, hex);
+            if (center.x < -hexSize.x || center.x > globalWidth + hexSize.x)
+                continue;
+            if (center.y < -hexSize.y || center.y > globalWidth + hexSize.y)
+                continue;
             ctx.beginPath();
             for (var i = 0; i < 6; i++) {
                 var pt = hex_corner_offset(layout, i);
@@ -80,38 +93,71 @@ function draw() {
             ctx.lineWidth = 4;
             ctx.stroke();
 
-            if (type == 'W')
-                ctx.fillStyle = '#8ED6FF';
-            else if (type == '0')
-                ctx.fillStyle = '#000';
-            else
-                ctx.fillStyle = '#8EFFD6';
-
+            ctx.fillStyle = cell.getColor();
             ctx.fill();
 
-            ctx.beginPath();
-            ctx.fillStyle = '#ff8E8E';
-            ctx.arc(center.x, center.y, 5, 0, 2*Math.PI);
-            ctx.closePath();
-            ctx.fill();
+            if (cell.moveEnabled) {
+                drawCenterCircle(center);
+            }
 
             if (showCoordsText) {
-                ctx.fillStyle = '#ffffff';
-                ctx.font = '1.1em Arial';
+                ctx.font = '1.0em Arial';
+                ctx.fillStyle = '#888';
                 var coordsText = q + "," + r;
                 var metrics = ctx.measureText(coordsText);
                 ctx.fillText(coordsText, center.x - (metrics.width/2), center.y);
             }
 
-            if (debug) {
-                var statusBarString = '';
-                timeToDraw = (timeToDraw * 0.9999) + ((Date.now() - startDraw) * 0.0001);
-                var fpsCount = Math.floor(1000 / timeToDraw);
-                statusBarString += 'Current FPS: ' + fpsCount;
-                drawStatusBar('Hexly (dev)', statusBarString);
-            }
         }
     }
+    if (debug) {
+        var rightText = '';
+        timeToDraw = (timeToDraw * 0.9999) + ((Date.now() - startDraw) * 0.0001);
+        var fpsCount = Math.floor(1000 / timeToDraw);
+        rightText += 'Current FPS: ' + fpsCount;
+
+        var leftText = 'Hexly (dev) | ';
+        if (currentCell)
+            leftText += 'current cell at (' + currentCell.q + ',' + currentCell.r + '): ' + currentCell.toString() + ' ';
+        if (showCoordsText)
+            leftText += 'show-coords ';
+        drawStatusBar(leftText, rightText);
+    }
+    needsUpdate = false;
+}
+
+function generateRandomMap() {
+    for (var i = 0; i < 15; i++) {
+        mapstore[i] = [];
+        for (var j = 0; j < 30; j++) {
+            var coord = new OffsetCoord(j, i);
+            var h = roffset_to_cube(ODD, coord);
+            var random = Math.floor((Math.random() * 10) + 1);
+            var cell = new MapCell(h.q, h.r, h.s, MapCell.Types.NONE);
+            if (random > 6) {
+                mapstore[i][j] = cell;
+                continue;
+            }
+            if (random > 6)
+                cell.type = MapCell.Types.WATER;
+            else
+                random = Math.floor((Math.random() * 10) + 1);
+                if (random > 1)
+                    cell.type = MapCell.Types.LAND;
+                else
+                    cell.type = MapCell.Types.MAGIC;
+
+            mapstore[i][j] = cell;
+        }
+    }
+}
+
+function drawCenterCircle(center) {
+    ctx.beginPath();
+    ctx.fillStyle = '#ff8E8E';
+    ctx.arc(center.x, center.y, 5, 0, 2*Math.PI);
+    ctx.closePath();
+    ctx.fill();
 }
 
 function drawStatusBar(leftText, rightText) {
@@ -125,40 +171,79 @@ function drawStatusBar(leftText, rightText) {
     ctx.strokeText(rightText, globalWidth - metrics.width - 10, globalHeight - 7);
 }
 
-function showMouseOver() {
-    showCoordsText = true;
+function mouseOver() {
 }
 
-function showMouseOut() {
-    showCoordsText = false;
-    canvas.onmousemove = null;
+function mouseOut() {
+    mouseDrag = false;
 }
 
 function mouseDown(e) {
-    canvas.onmousemove = mouseMove;
     mouseStart.x = e.clientX;
     mouseStart.y = e.clientY;
     oldPos.x = layout.origin.x;
     oldPos.y = layout.origin.y;
+    mouseDrag = true;
+    needsUpdate = true;
 }
 
 function mouseUp(e) {
-    canvas.onmousemove = null;
-    if (oldPos.x == layout.origin.x && oldPos.y == layout.origin.y) {
-        var h = hex_round(pixel_to_hex(layout, new Point(e.clientX, e.clientY)));
-        initmap.set(h.q, h.r, '0');
+    mouseDrag = false;
+    if (oldPos.x == layout.origin.x && oldPos.y == layout.origin.y && currentCell) {
+        if (selectedCell) {
+            for (var i = 0; i < hex_directions.length; i++) {
+                var neighbor = hex_neighbor(selectedCell, i);
+                var neighborcell = initmap.get(neighbor.q, neighbor.r);
+                if (neighborcell && neighborcell.type != MapCell.Types.NONE) {
+                    neighborcell.moveEnabled = false;
+                }
+            }
+        }
+        if (currentCell == selectedCell) {
+            selectedCell = false;
+        } else {
+            selectedCell = currentCell;
+            for (var i = 0; i < hex_directions.length; i++) {
+                var neighbor = hex_neighbor(selectedCell, i);
+                var neighborcell = initmap.get(neighbor.q, neighbor.r);
+                if (neighborcell && neighborcell.type != MapCell.Types.NONE) {
+                    neighborcell.moveEnabled = true;
+                }
+            }
+        }
     }
+    needsUpdate = true;
 }
 
 function mouseMove(e) {
-    var newPosX = oldPos.x - (mouseStart.x - e.clientX);
-    var newPosY = oldPos.y - (mouseStart.y - e.clientY);
-    var newPos = new Point(newPosX, newPosY);
+    if (mouseDrag) {
+        var newPosX = oldPos.x - (mouseStart.x - e.clientX);
+        var newPosY = oldPos.y - (mouseStart.y - e.clientY);
+        var newPos = new Point(newPosX, newPosY);
 
-    if (withinXBounds(newPos.x))
-        layout.origin.x = newPos.x;
-    if (withinYBounds(newPos.y))
-        layout.origin.y = newPos.y;
+        if (withinXBounds(newPos.x))
+            layout.origin.x = newPos.x;
+        if (withinYBounds(newPos.y))
+            layout.origin.y = newPos.y;
+    }
+
+    var h = hex_round(pixel_to_hex(layout, new Point(e.clientX, e.clientY)));
+    currentCell = initmap.get(h.q, h.r);
+    needsUpdate = true;
+}
+
+function keyUp(e) {
+    keyCodes[e.keyCode] = false;
+    needsUpdate = true;
+}
+
+function keyDown(e) {
+    if (e.keyCode == 67)
+        showCoordsText = !showCoordsText;
+    if (e.keyCode == 68)
+        debug = !debug;
+    keyCodes[e.keyCode] = true;
+    needsUpdate = true;
 }
 
 function resize() {
@@ -177,6 +262,7 @@ function resize() {
     if (!withinYBounds(layout.origin.y))
         layout.origin.y = hexSize.y;
         oldPos.y = hexSize.y;
+    needsUpdate = true;
     draw();
 }
 
