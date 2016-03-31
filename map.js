25a19f28d2ac295af949a845cc348d693d97e146
Serguey Parkhomovsky <xindigo@gmail.com>
2016-03-31 10:51:34 -0700

    fix getting nonexistent tiles when clicked

--- map.js
+++ map.js
@@ -6,10 +6,30 @@ function Map(store) {
     this.width = store[0].length;
 }
 
+Map.prototype.qrij = function(q, r) {
+    var i = r;
+    var j = Math.floor(q + r/2);
+    if (i < 0 || j < 0)
+        return false;
+    if (i >= this.height || j >= this.width)
+        return false;
+    return [i, j];
+}
+
 Map.prototype.get = function(q, r) {
-    return this.store[r][Math.floor(q + r/2)];
+    var ij = this.qrij(q, r);
+    if (!ij)
+        return false;
+    var i = ij[0];
+    var j = ij[1];
+    return this.store[i][j];
 }
 
 Map.prototype.set = function(q, r, val) {
-    this.store[r][Math.floor(q + r/2)] = val;
+    var ij = this.qrij(q, r);
+    if (!ij)
+        return false;
+    var i = ij[0];
+    var j = ij[1];
+    this.store[i][j] = val;
 }
